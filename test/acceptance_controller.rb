c81b0c0503c83ec330cf968ce7d640576a570750
James Coglan <jcoglan@googlemail.com>
2008-04-11 13:22:23 +0000

    Adding Ajax test and README.

--- test/acceptance_controller.rb
+++ test/acceptance_controller.rb
@@ -2,7 +2,8 @@ class AcceptanceController < ActionController::Base
   
   def create
     @user = AcceptanceUser.new
-    render :file => File.dirname(__FILE__) + '/create.html.erb'
+    render :file => File.dirname(__FILE__) + '/create.html.erb' and return unless request.xhr?
+    render :file => File.dirname(__FILE__) + '/xhr.html.erb'
   end
   
 end
